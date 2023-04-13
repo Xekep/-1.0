4dd696cf2abeeeea5c1487c51e25e3ae8766b498
Xekep <Xekep@users.noreply.github.com>
2023-04-13 11:38:57 +0300

    Update restapi.php

--- restapi.php
+++ restapi.php
@@ -8,7 +8,7 @@ class restapi
         $this->token = $token;
     }
     
-    function get_report(int $id): string
+    function get_report(int $id): ?string
     {
         $url = self::ARSHIN_BASE_URL . "api/applications/$id/protocol";
         $options = array('http' => array(
@@ -16,8 +16,8 @@ class restapi
             'header' => 'Authorization: Bearer '. $this->token
         ));
         $context  = stream_context_create($options);
-        $response = file_get_contents($url, false, $context);
-        return str_replace('gost:', '', $response);
+        $response = @file_get_contents($url, false, $context);
+        return !$response ? null : str_replace('gost:', '', $response);
     }
 
     function get_report_data(int $id): ?array
@@ -25,7 +25,9 @@ class restapi
         $verificationData = [];
 
         // Получение данных для формирования XML
-        $xml_protocol = new SimpleXMLElement($this->get_report($id));
+        $report = $this->get_report($id);
+        if(is_null($report)) return null;
+        $xml_protocol = new SimpleXMLElement($report);
         $records = $xml_protocol->children()->appProcessed->record;
 
         foreach ($records as $record) {
@@ -53,15 +55,15 @@ class restapi
         $response = file_get_contents($url, false, $context);
         return str_replace('gost:', '', $response);
     }
-    function verification(int $id): string
+    function verification(int $id): ?string
     {
         $url = self::ARSHIN_BASE_URL . "iaux/vri/$id";
         $options = array('http' => array(
             'method'  => 'GET'
         ));
         $context  = stream_context_create($options);
-        $response = file_get_contents($url, false, $context);
-        return $response;
+        $response = @file_get_contents($url, false, $context);
+        return !$response ? null : $response;
     }
 }
 ?>
